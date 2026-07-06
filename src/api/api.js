ffbf824b6db2fa97c7252602ce38780ac44389ef
kuzmavasil74 <98240149+kuzmavasil74@users.noreply.github.com>
2026-07-06 09:15:07 +0200

    Add VITE_API_URL env variable for production

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:4000'
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000'
 
 export const login = async (email, password) => {
   return await axios.post(`${API_URL}/auth/login`, {
